febdb841ff9e2035b9b190a835b568a2f2d2e05c
Brandon Jackson <brandonja991@gmail.com>
2023-03-29 07:20:01 -0500

    Add option and arg with testing

---
+++ src/OptionAndArgAccess.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ordinary\Command;
+
+use Ordinary\Command\Option\Parser;
+
+trait OptionAndArgAccess
+{
+    protected string $shortOps = '';
+
+    /** @var string[] */
+    protected array $longOpts = [];
+
+    protected bool $optionsOnlyAtStart = true;
+
+    /** @var array<string, string|array|false> */
+    private array $options = [];
+
+    /** @var string[] */
+    private array $args = [];
+
+    /** @var string[] */
+    private array $rawArgs = [];
+
+    /** @return array<string, string|array|false> */
+    final public function options(): array
+    {
+        return $this->options;
+    }
+
+    final public function option(string|int $name): string|false|array|null
+    {
+        return $this->options[$name] ?? null;
+    }
+
+    /**
+     * Get args re-indexed after options extracted
+     *
+     * @return string[]
+     */
+    final public function args(): array
+    {
+        return $this->args;
+    }
+
+    /** Get args re-indexed after options extracted */
+    final public function arg(int $index): ?string
+    {
+        return $this->args[$index] ?? null;
+    }
+
+    /**
+     * Get args indexed in original state before options parsed and extracted.
+     *
+     * @return string[]
+     */
+    final public function rawArgs(): array
+    {
+        return $this->rawArgs;
+    }
+
+    /**
+     * Get arg indexed in original state before options parsed and extracted.
+     */
+    final public function rawArg(int $index): ?string
+    {
+        return $this->rawArgs[$index] ?? null;
+    }
+
+    /** @param string[] $args */
+    public function withArgs(array $args): static
+    {
+        $new = clone $this;
+        $new->rawArgs = $args;
+        $new->parseOptions();
+
+        return $new;
+    }
+
+    protected function parseOptions(): void
+    {
+        $parser = new Parser($this->shortOps, $this->longOpts);
+        $parser->optsOnlyAtStart = $this->optionsOnlyAtStart;
+
+        $this->options = [];
+        $this->args = [];
+
+        foreach ($parser->parse($this->rawArgs()) as $option => $value) {
+            if (is_int($option)) {
+                $this->args[] = $value;
+
+                continue;
+            }
+
+            if (isset($this->options[$option])) {
+                if (!is_array($this->options[$option])) {
+                    $this->options[$option] = [$this->options[$option]];
+                }
+
+                $this->options[$option][] = $value;
+
+                continue;
+            }
+
+            $this->options[$option] = $value;
+        }
+    }
+}
---
+++ tests/OptionAndArgAccessTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ordinary\Command;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+
+class OptionAndArgAccessTest extends TestCase
+{
+    public static function withArgsProvider(): Generator
+    {
+        yield [['foo'], [], ['foo']];
+
+        yield [
+            ['-f', '-b', 'b1', '-zz1', '--foo', '--bar', 'bar1', '--baz=baz1', 'foo'],
+            [
+                'f' => false,
+                'b' => 'b1',
+                'z' => 'z1',
+                'foo' => false,
+                'bar' => 'bar1',
+                'baz' => 'baz1',
+            ],
+            ['foo'],
+        ];
+
+        yield [
+            [
+                '-f', '-f',
+                '-b', 'b1', '-bb2', '-b=b3',
+                '-zz2', '-z=z3', '-z',
+                '--foo', '--foo',
+                '--bar', 'bar2', '--bar=bar3',
+                '--baz=baz1', '--baz',
+                'foo', 'bar', 'baz',
+            ],
+            [
+                'f' => [false, false],
+                'b' => ['b1', 'b2', 'b3'],
+                'z' => ['z2', 'z3', false],
+                'foo' => [false, false],
+                'bar' => ['bar2', 'bar3'],
+                'baz' => ['baz1', false],
+            ],
+            ['foo', 'bar', 'baz'],
+        ];
+    }
+
+    /**
+     * @param string[] $args
+     * @param array<string, string[]|string> $expectedOptions
+     * @param string[] $expectedArgs
+     * @dataProvider withArgsProvider
+     */
+    public function testWithArgs(array $args, array $expectedOptions, array $expectedArgs): void
+    {
+        $obj = new class () {
+            use OptionAndArgAccess;
+
+            public function __construct()
+            {
+                $this->shortOps = 'fb:z::';
+                $this->longOpts = ['foo', 'bar:', 'baz::'];
+            }
+        };
+
+        $a = $obj->withArgs($args);
+
+        self::assertSame($args, $a->rawArgs());
+        self::assertEquals($expectedOptions, $a->options());
+        self::assertSame($expectedArgs, $a->args());
+
+        foreach ($expectedOptions as $opt => $optValue) {
+            self::assertSame($optValue, $a->option($opt));
+        }
+
+        foreach ($expectedArgs as $argI => $argV) {
+            self::assertSame($argV, $a->arg($argI));
+        }
+
+        foreach ($args as $rawArgI => $rawArgV) {
+            self::assertSame($rawArgV, $a->rawArg($rawArgI));
+        }
+    }
+}
