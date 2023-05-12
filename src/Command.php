2062c36465c559d073d1ab6f8f41892753b08964
Brandon Jackson <brandonja991@gmail.com>
2023-05-12 13:07:31 -0500

    Rm from args magic constructor

--- src/Command.php
+++ src/Command.php
@@ -13,21 +13,6 @@ abstract class Command
 
     abstract public function showHelp(): void;
 
-    /**
-     * @param string[] $args
-     * @param mixed ...$params Params passed to constructor.
-     */
-    public static function fromArgs(array $args = [], mixed ...$params): static
-    {
-        /**
-         * @psalm-suppress TooManyArguments
-         * @psalm-suppress UnsafeInstantiation
-         */
-        $obj = new static(...$params);
-
-        return $obj->withArgs($args);
-    }
-
     public function beforeExecute(): ?int
     {
         return null;
