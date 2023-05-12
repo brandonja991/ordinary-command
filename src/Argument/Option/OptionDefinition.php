f880e4ae3666621b5ba8955aded8b979ec44b291
Brandon Jackson <brandonja991@gmail.com>
2023-05-12 13:04:00 -0500

    Add option definition and tests

---
+++ src/Argument/Option/OptionDefinition.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ordinary\Command\Argument\Option;
+
+use Ordinary\Command\UnexpectedValueException;
+
+final class OptionDefinition
+{
+    public readonly OptionType $type;
+
+    public static function fromString(string $definition): self
+    {
+        $valueRequirement = ValueRequirement::fromDefinitionString($definition);
+        $name = $valueRequirement->extractName($definition);
+
+        return new self($name, $valueRequirement);
+    }
+
+    /** @param string[] $aliases */
+    public function __construct(
+        public readonly string $name,
+        public readonly ValueRequirement $valueRequirement = ValueRequirement::None,
+        public readonly array $aliases = [],
+        public readonly string $description = '',
+    ) {
+        $this->type = OptionType::fromName($this->name);
+    }
+
+    /**
+     * @param string[] $longOpts
+     * @return array<string, self>
+     */
+    public static function fromDefinitions(string $shortOpts = '', array $longOpts = []): array
+    {
+        $shortOptsObjects = array_map(self::fromString(...), self::splitShortOpts($shortOpts));
+        $longOptsObjects = array_map(self::fromString(...), $longOpts);
+
+        $shortOptsByName = array_combine(
+            array_column($shortOptsObjects, 'name'),
+            $shortOptsObjects,
+        );
+        $longOptsByName = array_combine(
+            array_column($longOptsObjects, 'name'),
+            $longOptsObjects,
+        );
+
+        if ($duplicates = array_intersect_key($shortOptsByName, $longOptsByName)) {
+            throw new UnexpectedValueException(
+                'Duplicate long and short option definitions found: '
+                . implode(', ', array_keys($duplicates)),
+            );
+        }
+
+        $merged = array_merge($shortOptsByName, $longOptsByName);
+        ksort($merged);
+
+        return $merged;
+    }
+
+    /** @return string[] */
+    public static function splitShortOpts(string $shortOpts): array
+    {
+        return $shortOpts === '' ? [] : preg_split('/(?<=.)(?=[^:])/', $shortOpts);
+    }
+}
---
+++ tests/Argument/Option/OptionDefinitionTest.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ordinary\Command\Argument\Option;
+
+use Generator;
+use Ordinary\Command\UnexpectedValueException;
+use PHPUnit\Framework\TestCase;
+use Throwable;
+
+class OptionDefinitionTest extends TestCase
+{
+    public static function fromDefinitionsProvider(): Generator
+    {
+        yield ['f', [], ['f' => new OptionDefinition('f', ValueRequirement::None)]];
+        yield ['', ['foo'], ['foo' => new OptionDefinition('foo', ValueRequirement::None)]];
+
+        yield [
+            'abc',
+            [],
+            [
+                'a' => new OptionDefinition('a', ValueRequirement::None),
+                'b' => new OptionDefinition('b', ValueRequirement::None),
+                'c' => new OptionDefinition('c', ValueRequirement::None),
+            ],
+        ];
+
+        yield [
+            'ab:c::d::e:f',
+            [],
+            [
+                'a' => new OptionDefinition('a', ValueRequirement::None),
+                'b' => new OptionDefinition('b', ValueRequirement::Required),
+                'c' => new OptionDefinition('c', ValueRequirement::Optional),
+                'd' => new OptionDefinition('d', ValueRequirement::Optional),
+                'e' => new OptionDefinition('e', ValueRequirement::Required),
+                'f' => new OptionDefinition('f', ValueRequirement::None),
+            ],
+        ];
+
+        yield [
+            '',
+            ['foo', 'bar:', 'baz::'],
+            [
+                'bar' => new OptionDefinition('bar', ValueRequirement::Required),
+                'baz' => new OptionDefinition('baz', ValueRequirement::Optional),
+                'foo' => new OptionDefinition('foo', ValueRequirement::None),
+            ],
+        ];
+
+        yield [
+            'ab:c::',
+            ['foo', 'bar:', 'baz::'],
+            [
+                'a' => new OptionDefinition('a', ValueRequirement::None),
+                'b' => new OptionDefinition('b', ValueRequirement::Required),
+                'bar' => new OptionDefinition('bar', ValueRequirement::Required),
+                'baz' => new OptionDefinition('baz', ValueRequirement::Optional),
+                'c' => new OptionDefinition('c', ValueRequirement::Optional),
+                'foo' => new OptionDefinition('foo', ValueRequirement::None),
+            ],
+        ];
+
+        yield ['f', ['f'], UnexpectedValueException::class];
+    }
+
+    /**
+     * @param string[] $longOpts
+     * @param class-string<Throwable>|array<string|int, OptionDefinition> $expected
+     * @dataProvider fromDefinitionsProvider
+     */
+    public function testFromDefinitions(string $shortOpts, array $longOpts, string|array $expected): void
+    {
+        if (!is_array($expected)) {
+            self::expectException($expected);
+        }
+
+        $result = OptionDefinition::fromDefinitions($shortOpts, $longOpts);
+
+        self::assertEquals($expected, $result);
+    }
+
+    public function testFromString(): void
+    {
+        $long = OptionDefinition::fromString('foo');
+        self::assertSame(OptionType::Long, $long->type);
+
+        $short = OptionDefinition::fromString('f');
+        self::assertSame(OptionType::Short, $short->type);
+
+        self::expectException(UnexpectedValueException::class);
+        OptionDefinition::fromString('');
+    }
+
+    public function testSplitShortOpts(): void
+    {
+        self::assertSame([], OptionDefinition::splitShortOpts(''));
+
+        self::assertSame(['a'], OptionDefinition::splitShortOpts('a'));
+        self::assertSame(['a:'], OptionDefinition::splitShortOpts('a:'));
+        self::assertSame(['a::'], OptionDefinition::splitShortOpts('a::'));
+        self::assertSame(['a:::'], OptionDefinition::splitShortOpts('a:::'));
+
+        self::assertSame(['a', 'b', 'c'], OptionDefinition::splitShortOpts('abc'));
+
+        self::assertSame(['a:', 'b', 'c'], OptionDefinition::splitShortOpts('a:bc'));
+        self::assertSame(['a', 'b:', 'c'], OptionDefinition::splitShortOpts('ab:c'));
+        self::assertSame(['a', 'b', 'c:'], OptionDefinition::splitShortOpts('abc:'));
+        self::assertSame(['a:', 'b:', 'c'], OptionDefinition::splitShortOpts('a:b:c'));
+        self::assertSame(['a', 'b:', 'c:'], OptionDefinition::splitShortOpts('ab:c:'));
+        self::assertSame(['a:', 'b', 'c:'], OptionDefinition::splitShortOpts('a:bc:'));
+
+        self::assertSame(['a::', 'b', 'c'], OptionDefinition::splitShortOpts('a::bc'));
+        self::assertSame(['a', 'b::', 'c'], OptionDefinition::splitShortOpts('ab::c'));
+        self::assertSame(['a', 'b', 'c::'], OptionDefinition::splitShortOpts('abc::'));
+        self::assertSame(['a::', 'b::', 'c'], OptionDefinition::splitShortOpts('a::b::c'));
+        self::assertSame(['a', 'b::', 'c::'], OptionDefinition::splitShortOpts('ab::c::'));
+        self::assertSame(['a::', 'b', 'c::'], OptionDefinition::splitShortOpts('a::bc::'));
+
+        self::assertSame(['a:::', 'b', 'c'], OptionDefinition::splitShortOpts('a:::bc'));
+        self::assertSame(['a', 'b:::', 'c'], OptionDefinition::splitShortOpts('ab:::c'));
+        self::assertSame(['a', 'b', 'c:::'], OptionDefinition::splitShortOpts('abc:::'));
+        self::assertSame(['a:::', 'b:::', 'c'], OptionDefinition::splitShortOpts('a:::b:::c'));
+        self::assertSame(['a', 'b:::', 'c:::'], OptionDefinition::splitShortOpts('ab:::c:::'));
+        self::assertSame(['a:::', 'b', 'c:::'], OptionDefinition::splitShortOpts('a:::bc:::'));
+    }
+}
